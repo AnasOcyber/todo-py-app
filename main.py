888956b1174e6166b0ff4acb6757f01324658cb7
Anas Obaid <anasobaid@ocyber.io>
2023-12-06 17:55:21 +0500

    Add time stamps to track task completetion time.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-# use namedtuple
+from datetime import datetime
+
+
 class Task:
     def __init__(self, description) -> None:
         self.id = len(all_tasks) + 1
         self.description = description
         self.status = "pending"
+        self.start_time = datetime.now()
 
 
 def view_tasks():
@@ -17,7 +20,7 @@ def view_tasks():
         print("No tasks found.\n")
 
 
-def add_task(description):
+def add_task():
     # Creating and adding new task object to the all_tasks list
     description = input("Description: ")
     try:
@@ -34,10 +37,14 @@ def add_task(description):
 def mark_completed():
     task_id = int(input("Id: "))
     for task in all_tasks:
-        if task.id == task_id:
-            task.status = "completed"
-    else:
-        print(f"We couldn't find task with '{task_id}' id\n")
+        if all_tasks != []:
+            if task.id == task_id:
+                task.status = "completed"
+                print("Status changed!")
+                print(f"Completed in {
+                      (datetime.now() - task.start_time)} time")
+        else:
+            print(f"We couldn't find task with '{task_id}' id\n")
 
 
 def show_menu():
